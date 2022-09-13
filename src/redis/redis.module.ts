7e6da07e54c1019e2f1073e0897dcb737de08ffc
dkhorev <me@dkhorev.pro>
2022-09-13 02:02:32 -0400

    redis demo updated

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,20 +1,27 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { RedisController } from './redis.controller';
 import { RedisService } from './redis.service';
 
 @Module({
   imports: [
-    BullModule.forRoot({
-      redis: {
-        host: process.env.REDIS_HOST || 'localhost',
-        port: +process.env.REDIS_PORT || 6379,
-      },
-      defaultJobOptions: {
-        timeout: 1000,
-        removeOnComplete: true,
-        removeOnFail: true,
-      },
+    ConfigModule.forRoot(),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        redis: {
+          host: config.get('REDIS_HOST') || 'localhost',
+          port: +config.get('REDIS_PORT') || 6379,
+          password: config.get('REDIS_PASSWORD') || undefined,
+        },
+        defaultJobOptions: {
+          timeout: 1000,
+          removeOnComplete: true,
+          removeOnFail: true,
+        },
+      }),
     }),
     BullModule.registerQueue({
       name: 'QUEUE_DEMO',
